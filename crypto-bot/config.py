33c1fff0bd814a9aef27ade4bd69b21db8818827
bk kim <ku0815@nate.com>
2025-05-22 13:56:26 +0900

    config.py 업데이트

--- crypto-bot/config.py
+++ crypto-bot/config.py
@@ -17,7 +17,7 @@ SPIKE_CONFIG = {
     
     
     # 알림 제어
-    "notify_on_spike_fail": False,
+    "notify_on_spike_fail": True,
     "notify_on_disparity_fail": False,
     "notify_on_error": True
 

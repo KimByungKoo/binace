c86071aebe8b8ca9bef9e9f6469d6ec3362db3a5
bk kim <ku0815@nate.com>
2025-05-22 12:56:47 +0900

    binance.py 업데이트

--- crypto-bot/utils/binance.py
+++ crypto-bot/utils/binance.py
@@ -1,6 +1,15 @@
 from binance.client import Client
 import pandas as pd
+
+from dotenv import load_dotenv
+import requests
+import time
 import os
+
+load_dotenv()
+
+
+
 
 API_KEY = os.getenv("API_KEY")
 API_SECRET = os.getenv("API_SECRET")

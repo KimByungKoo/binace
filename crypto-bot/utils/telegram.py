8067e2914e1a9dbd87fa87b247c95548a59b0c4f
bk kim <ku0815@nate.com>
2025-05-22 12:57:03 +0900

    telegram.py 업데이트

--- crypto-bot/utils/telegram.py
+++ crypto-bot/utils/telegram.py
@@ -1,5 +1,12 @@
 import requests
+from dotenv import load_dotenv
+import requests
+import time
 import os
+
+load_dotenv()
+
+
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")

6cf366aad77b5a695d069cbcf4d64151b5c2e29e
bk kim <ku0815@nate.com>
2025-05-27 01:41:44 +0900

    spike_disparity.py 업데이트

--- crypto-bot/strategy/spike_disparity.py
+++ crypto-bot/strategy/spike_disparity.py
@@ -7,7 +7,7 @@ from order_manager import auto_trade_from_signal
 import pandas as pd
 
 def check_volume_spike_disparity(symbol):
-    issues = []  # 실패 이유 리스트
+    issues = [] 
 
     try:
         if not symbol:

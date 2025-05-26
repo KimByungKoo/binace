0e89124e87831c92dc4875b23021abbde40685c1
bk kim <ku0815@nate.com>
2025-05-27 06:18:58 +0900

    config.py 업데이트

--- crypto-bot/config.py
+++ crypto-bot/config.py
@@ -23,8 +23,8 @@ SPIKE_CONFIG = {
     "reverse_trade": True,      # 반대방향 진입 여부
 
 
-    "rsi_period": 14,
-    "rsi_threshold": 10,
+    "rsi_period":7,
+    "rsi_threshold": 20,
 
     # 가격 기울기
     "price_lookback": 5,

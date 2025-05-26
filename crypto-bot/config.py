e6f6ea358c09de765d1649276ebe4ba7fc85e7d1
bk kim <ku0815@nate.com>
2025-05-26 10:41:27 +0900

    config.py 업데이트

--- crypto-bot/config.py
+++ crypto-bot/config.py
@@ -14,6 +14,12 @@ SPIKE_CONFIG = {
     
     "volatility_multiplier": 2.0,    # 과거 평균 변동률의 N배 이상이어야 spike로 인정
     
+    
+    "volume_spike_multiplier": 2.5,
+    "min_disparity_pct": 0.3,
+    "ma_periods": [7, 20, 30, 60],
+    "require_alignment": True,  # 정배열/역배열 필터링 유무
+    "reverse_trade": True,      # 반대방향 진입 여부
 
 
     # 가격 기울기

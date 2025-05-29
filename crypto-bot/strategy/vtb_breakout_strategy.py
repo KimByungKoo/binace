dbf75126e246d350315214df2e17ef0adc56f188
bk kim <ku0815@nate.com>
2025-05-30 07:27:47 +0900

    vtb_breakout_strategy.py 업데이트

--- crypto-bot/strategy/vtb_breakout_strategy.py
+++ crypto-bot/strategy/vtb_breakout_strategy.py
@@ -4,8 +4,8 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 from utils.telegram import send_telegram_message
-from utils.binance import get_1m_klines, has_open_position, place_order, set_leverage
-from order_manager import auto_trade_from_signal
+from utils.binance import get_1m_klines, has_open_position
+from order_manager import auto_trade_from_signal, place_order, set_leverage
 
 # === CONFIG ===
 CONFIG = {
